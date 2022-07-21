b0d1e0970f51879800e6fdc62247368dcd0698e7
monk-ee <magic.monkee.magic@gmail.com>
2022-07-21 15:05:25 +1000

    feat: refactor for a complete example

--- cert.tf
+++ cert.tf
@@ -4,14 +4,14 @@ resource "aws_route53_record" "a_record" {
   type    = "A"
   alias {
     evaluate_target_health = false
-    name = module.aws_alb_https.alb_dns_name
-    zone_id = module.aws_alb_https.alb_zone_id
+    name                   = module.aws_alb_https.alb_dns_name
+    zone_id                = module.aws_alb_https.alb_zone_id
   }
   depends_on = [module.aws_alb_https]
 }
 
 resource "aws_acm_certificate" "domain_cert" {
-  domain_name       = var.common_name
+  domain_name               = var.common_name
   certificate_authority_arn = var.certificate_authority_arn
   lifecycle {
     create_before_destroy = true
--- service-defnition.tf
+++ service-defnition.tf
@@ -1,28 +1,30 @@
 # ====================== SERVICE ==========================
 resource "aws_ecs_service" "ecs_service" {
   depends_on = [
-    aws_ecs_task_definition.ecs_task_definition, module.aws_alb_https]
+    aws_ecs_task_definition.ecs_task_definition, module.aws_alb_https
+  ]
   #aws_iam_role_policy
-  name = format("%s-service-%s", var.workload, var.short_region)
-  cluster = var.cluster_id
-  launch_type = var.default_capacity_provider
+  name            = format("service-%s-%s", var.workload, var.short_region)
+  cluster         = var.cluster_id
+  launch_type     = var.default_capacity_provider
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-  desired_count = var.service_replicas
+  desired_count   = var.service_replicas
 
   load_balancer {
-    container_name = module.this.id
-    container_port = var.container_port
+    container_name   = module.this.id
+    container_port   = var.container_port
     target_group_arn = module.aws_alb_https.default_target_group_arn
   }
 
   network_configuration {
-    security_groups = var.private_security_group_ids
-    subnets = var.non_routable_subnet_ids
+    security_groups = var.non_routable_security_group_ids
+    subnets         = var.non_routable_subnet_ids
   }
 
   lifecycle {
     ignore_changes = [
-      desired_count]
+      desired_count
+    ]
   }
-  tags = module.this.tags
+  context = module.this.context
 }
